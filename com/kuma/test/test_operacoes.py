bf72824c50afcd8f1f98abf53e03f19b75eacb92
kiss107 <68926649+kiss107@users.noreply.github.com>
2020-10-14 12:09:21 -0300

    adicionando conteudo no arquivo

--- com/kuma/test/test_operacoes.py
+++ com/kuma/test/test_operacoes.py
@@ -16,10 +16,10 @@ def test_soma():
     assert somar(4,6) == 10
 
 def test_subtrair():
-    assert subtrair(10,5)
+    assert subtrair(10,5) == 5
 
 def test_multiplicacao():
-    assert multiplicacao(20,3)
+    assert multiplicacao(20,3) == 60
 
 def  test_divisao():
-    assert divisao(20,5)
+    assert divisao(20,5) == 4
---
+++ com/kuma/teste.py
@@ -0,0 +1,15 @@
+import pytest
+
+from com.kuma.test.principal import *
+
+def test_somar():
+        assert somar(5,6) == 11
+
+def test_subtrair():
+        assert subtrair(8,3) == 5 
+
+def test_multiplicacao():
+        assert multiplicacao(50,5) == 250
+
+def test_divisao():
+        assert divisao(60,6) == 10
